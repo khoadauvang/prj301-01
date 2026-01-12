712a6a6abdb1e0ba4a777c37ac947841d3732ed5
khoadauvang <khoadenhat26092005@gmail.com>
2026-01-12 16:27:32 +0700

    update

---
+++ Web_03_a/src/java/models/UserDTO.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author DELL
+ */
+public class UserDTO {
+    private String username;
+    private String password;
+    private String fullname;
+    
+    public UserDTO() {
+        //
+    }
+    
+    public UserDTO(String username, String password, String fullname) {
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+}
